a8e0c07f8be51beb692b6cd20449a64e856d47c1
AadilKhatri <38430842+AadilKhatri@users.noreply.github.com>
2018-09-04 22:57:04 -0500

    Updated IEmployee

---
+++ IEmployee.java
@@ -0,0 +1,13 @@
+/*
+This class is a employee class which is an implementation of Iworker class.
+Which has a method of eat.
+
+*/
+
+class IEmployee implements IWorker{
+
+	public void eat() {
+		//.... eating in launch break
+	}
+
+}
