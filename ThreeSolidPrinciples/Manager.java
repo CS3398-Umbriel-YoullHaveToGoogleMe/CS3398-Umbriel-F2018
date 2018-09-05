3370f2ee402c5bb3c8246802e321a339c156bf8b
Salvador Romero <s_r273@txstate.edu>
2018-09-05 09:38:05 -0500

    Manager.java

--- ThreeSolidPrinciples/Manager.java
+++ ThreeSolidPrinciples/Manager.java
@@ -1,6 +1,8 @@
-/* Manger Seperated from the rest so that it follows Single Responsiblity
-pricniple and it can be changed if needed */
-import threesolid;
+/*Seperating the manager class follows the Open Close prinicple becasue it makes it easy to 
+add to the file without changing the code in other files. It also follows the Single Responsiblilty
+principle since we can have one person working on this file if needed. It does not follow the Interface
+Segregation principle*/
+package threesolid;
 
 class Manager {
 	IWorker worker;
