2081892292aefa176349c20bd4a98f4adc50b218
Salvador Romero <s_r273@txstate.edu>
2018-09-05 09:26:55 -0500

    misspelling of a work

--- ThreeSolidPrinciples/Main.java
+++ ThreeSolidPrinciples/Main.java
@@ -1,7 +1,7 @@
 /* We seperated the main class from the rest of the files so we can mainly follow the Open Close principle.
 Since we will be mostly making modifications to other files, we kept this file seperate from others so that
 it is still open to modifications if needed. Since this files modification may depend on many factors then we
-may not be able to apply the single responsilbity principle here. Interface Segregation does not aplly to this file*/
+may not be able to apply the single responsilbity principle here. Interface Segregation does not apply to this file*/
 
 package threesolid;
 
