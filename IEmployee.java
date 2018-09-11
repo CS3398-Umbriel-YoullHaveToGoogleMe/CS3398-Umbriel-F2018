f4b38e85494edf92d0e91429e50384643e863ec6
StevenHernandezJr <42784972+StevenHernandezJr@users.noreply.github.com>
2018-09-11 09:02:25 -0500

    IEmployee

--- IEmployee.java
+++ IEmployee.java
@@ -1,3 +1,9 @@
+/*
+This class is a employee interface which is seperate from the IWorker interface.
+IEmployee has a method of eat.
+*/
+
+
 public interface IEmployee {
 	public void eat() {
 		//.... eating in launch break
